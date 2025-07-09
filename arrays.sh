05ad12322beee27cbeed51f4a5f7645a3eeef4f1
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2025-07-09 20:31:21 +0530

    arrays

---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this is called array, instead of single value it can hold multiple values
+
+persons=("ramesh" "suresh" "amla")
+
+echo "first person: ${person[0]}"
+echo " all personss: ${person[@]}"
