a9e43ab11626724aac0f93df0ca5aa9ba3ed352e
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2025-07-09 20:22:42 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "please enter username"
+read username
+
+echo "Please enter your password"
+
+#this is only for validation, we should not print in the terminal
+read -s password
+
+echo "password enered is : $password"
