ae224ec0db09126a1c2430be6f6de9dce1d3a963
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2025-07-09 19:55:53 +0530

    variables

---
+++ 01-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Date=$(date)
+echo "this script is executed at time stamp : $Date"
+
+
+
---
+++ helloworld.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+echo "hello world , Iam learning AWS DEVOPS"
+person1=siva
+person2=sree
+
+echo "$person1: hi , GM"
+echo "$person2: gm $person1 how r u "
+echo "$person1: Im gd"
+
+
