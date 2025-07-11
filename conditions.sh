2fabf398c1ea0272e753ad9013b4fdc108138d58
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2025-07-11 15:51:46 +0530

    conditions

---
+++ conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# check if the number is greater than 10 or not
+Number=$1
+
+if [$Number -gt 10]
+then 
+    echo "$Number is greater than 10 "
+else
+    echo  "$Number is not  greater than 10 "
+fi 
